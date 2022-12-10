0e56fa613515c25fc229d0eaae6d056f3dee4b31
MR-BOTZt <120255966+MR-BOTZt@users.noreply.github.com>
2022-12-10 23:41:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -90,7 +90,7 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ 𝙰𝚍𝚍 𝙼𝚎 𝚃𝚘 𝚈𝚘𝚞𝚛 𝙶𝚛𝚘𝚞𝚙𝚜 ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 𝚂𝚎𝚊𝚛𝚌𝚑', switch_inline_query_cswitch_inline_query_current_chat=''urrent_chat=''),
+            InlineKeyboardButton('🔍 𝚂𝚎𝚊𝚛𝚌𝚑', switch_inline_query_cswitch_inline_query_current_chat='urrent_chat='),
             InlineKeyboardButton('🤖 𝚄𝚙𝚍𝚊𝚝𝚎𝚜', url='https://t.me/CINEMAHUB_NEW_MOVIES')
             ],[
             InlineKeyboardButton('ℹ️ 𝙷𝚎𝚕𝚙', callback_data='help'),
